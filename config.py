71e2f1c878f3c7f582a5d6079dc72802142847d7
Vishal Adithya.A <vishaladithya2008@gmail.com>
2026-06-22 13:00:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 from langchain_google_genai import GoogleGenerativeAI
 
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 llm = GoogleGenerativeAI(
     model="gemini-2.5-flash-lite"
     )
